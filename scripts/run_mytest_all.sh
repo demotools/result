db72b040710bd65cab2299bc4f5db542f00e6da0
demotools <quhongliang1984@163.com>
2020-12-01 00:10:36 +0800

    Update run_mytest_all.sh

--- scripts/run_mytest_all.sh
+++ scripts/run_mytest_all.sh
@@ -23,9 +23,9 @@ ROOT=$(dirname `readlink -f "$0"`)
 BENCHMARKS="btree"
 # List of all configs to run
 #CONFIGS="F FM FA FAM I IM"
-CONFIGS="F"
+CONFIGS="FM"
 
-for RUNTIMES in $(seq 1) 
+for RUNTIMES in $(seq 10) 
 do
 	echo "Start test round: $RUNTIMES"
 	echo "Start test round: $RUNTIMES" >> /var/log/syslog 
