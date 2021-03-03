294ed5ea30043acbaa50341f84bf69c54873800f
demotools <quhongliang1984@163.com>
2021-03-03 23:31:57 +0800

    Update run_mytest_all.sh

--- scripts/run_mytest_all.sh
+++ scripts/run_mytest_all.sh
@@ -19,18 +19,19 @@ ROOT=$(dirname `readlink -f "$0"`)
 # List of all benchmarks to run
 #BENCHMARKS="memcached xsbench graph500 hashjoin btree canneal"
 #BENCHMARKS="btree hashjoin"
-BENCHMARKS="hashjoin"
-#BENCHMARKS="btree"
+#BENCHMARKS="hashjoin"
+BENCHMARKS="btree"
 # List of all configs to run
 #CONFIGS="F FM FA FAM I IM"
 #CONFIGS="F"
-CONFIGS="FM"
+#CONFIGS="FM"
 #CONFIGS="I"
 #CONFIGS="IM"
 #CONFIGS="F FM I IM"
 #CONFIGS="F FM"
+CONFIGS="FM F"
 
-for RUNTIMES in $(seq 1) 
+for RUNTIMES in $(seq 10) 
 do
 	echo "Start test round: $RUNTIMES"
 	echo "Start test round: $RUNTIMES" >> /var/log/syslog 
