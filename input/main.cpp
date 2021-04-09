da1c6afe8d34874e84521e4d4cb11bb539b5c4e7
demotools <quhongliang1984@163.com>
2021-04-09 10:54:48 +0800

    Update main.cpp

--- input/main.cpp
+++ input/main.cpp
@@ -27,6 +27,119 @@
 // OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
 
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <vector>
+
+#ifdef ENABLE_THREADS
+#include <pthread.h>
+#endif
+
+#ifdef ENABLE_PARSEC_HOOKS
+#include <hooks.h>
+#endif
+
+#include "annealer_types.h"
+#include "annealer_thread.h"
+#include "netlist.h"
+#include "rng.h"
+
+using namespace std;
+
+void* entry_pt(void*);
+
+
+// int real_main (int argc, char * const argv[]);
+int real_main (int argc, char * const argv[]) {
+#ifdef PARSEC_VERSION
+#define __PARSEC_STRING(x) #x
+#define __PARSEC_XSTRING(x) __PARSEC_STRING(x)
+        cout << "PARSEC Benchmark Suite Version "__PARSEC_XSTRING(PARSEC_VERSION) << endl << flush;
+#else
+        cout << "PARSEC Benchmark Suite" << endl << flush;
+#endif //PARSEC_VERSION
+#ifdef ENABLE_PARSEC_HOOKS
+	__parsec_bench_begin(__parsec_canneal);
+#endif
+
+	srandom(3);
+
+	if(argc != 5 && argc != 6) {
+		cout << "Usage: " << argv[0] << " NTHREADS NSWAPS TEMP NETLIST [NSTEPS]" << endl;
+		exit(1);
+	}	
+	
+	//argument 1 is numthreads
+	int num_threads = atoi(argv[1]);
+	cout << "Threadcount: " << num_threads << endl;
+#ifndef ENABLE_THREADS
+	if (num_threads != 1){
+		cout << "NTHREADS must be 1 (serial version)" <<endl;
+		exit(1);
+	}
+#endif
+		
+	//argument 2 is the num moves / temp
+	int swaps_per_temp = atoi(argv[2]);
+	cout << swaps_per_temp << " swaps per temperature step" << endl;
+
+	//argument 3 is the start temp
+	int start_temp =  atoi(argv[3]);
+	cout << "start temperature: " << start_temp << endl;
+	
+	//argument 4 is the netlist filename
+	string filename(argv[4]);
+	cout << "netlist filename: " << filename << endl;
+	
+	//argument 5 (optional) is the number of temperature steps before termination
+	int number_temp_steps = -1;
+        if(argc == 6) {
+		number_temp_steps = atoi(argv[5]);
+		cout << "number of temperature steps: " << number_temp_steps << endl;
+        }
+
+	//now that we've read in the commandline, run the program
+	netlist my_netlist(filename);
+	
+	annealer_thread a_thread(&my_netlist,num_threads,swaps_per_temp,start_temp,number_temp_steps);
+	
+#ifdef ENABLE_PARSEC_HOOKS
+	__parsec_roi_begin();
+#endif
+#ifdef ENABLE_THREADS
+	std::vector<pthread_t> threads(num_threads);
+	void* thread_in = static_cast<void*>(&a_thread);
+	for(int i=0; i<num_threads; i++){
+		pthread_create(&threads[i], NULL, entry_pt,thread_in);
+	}
+	for (int i=0; i<num_threads; i++){
+		pthread_join(threads[i], NULL);
+	}
+#else
+	a_thread.Run();
+#endif
+#ifdef ENABLE_PARSEC_HOOKS
+	__parsec_roi_end();
+#endif
+	
+	cout << "Final routing is: " << my_netlist.total_routing_cost() << endl;
+
+#ifdef ENABLE_PARSEC_HOOKS
+	__parsec_bench_end();
+#endif
+
+	return 0;
+}
+
+void* entry_pt(void* data)
+{
+	annealer_thread* ptr = static_cast<annealer_thread*>(data);
+	ptr->Run();
+}
+
+
 #include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -60,7 +173,7 @@
 FILE *opt_file_out = NULL;  ///< standard outpu
 
 // extern int real_main(int argc, char *argv[]);
-int real_main (int argc, char * const argv[]);
+
 
 void signalhandler(int sig)
 {
@@ -178,116 +291,4 @@ int main(int argc, char *argv[])
 
     fprintf(opt_file_out, "</benchmark>\n");
     return 0;
-}
-
-#include <iostream>
-#include <math.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <vector>
-
-#ifdef ENABLE_THREADS
-#include <pthread.h>
-#endif
-
-#ifdef ENABLE_PARSEC_HOOKS
-#include <hooks.h>
-#endif
-
-#include "annealer_types.h"
-#include "annealer_thread.h"
-#include "netlist.h"
-#include "rng.h"
-
-using namespace std;
-
-void* entry_pt(void*);
-
-
-int real_main (int argc, char * const argv[]);
-int real_main (int argc, char * const argv[]) {
-#ifdef PARSEC_VERSION
-#define __PARSEC_STRING(x) #x
-#define __PARSEC_XSTRING(x) __PARSEC_STRING(x)
-        cout << "PARSEC Benchmark Suite Version "__PARSEC_XSTRING(PARSEC_VERSION) << endl << flush;
-#else
-        cout << "PARSEC Benchmark Suite" << endl << flush;
-#endif //PARSEC_VERSION
-#ifdef ENABLE_PARSEC_HOOKS
-	__parsec_bench_begin(__parsec_canneal);
-#endif
-
-	srandom(3);
-
-	if(argc != 5 && argc != 6) {
-		cout << "Usage: " << argv[0] << " NTHREADS NSWAPS TEMP NETLIST [NSTEPS]" << endl;
-		exit(1);
-	}	
-	
-	//argument 1 is numthreads
-	int num_threads = atoi(argv[1]);
-	cout << "Threadcount: " << num_threads << endl;
-#ifndef ENABLE_THREADS
-	if (num_threads != 1){
-		cout << "NTHREADS must be 1 (serial version)" <<endl;
-		exit(1);
-	}
-#endif
-		
-	//argument 2 is the num moves / temp
-	int swaps_per_temp = atoi(argv[2]);
-	cout << swaps_per_temp << " swaps per temperature step" << endl;
-
-	//argument 3 is the start temp
-	int start_temp =  atoi(argv[3]);
-	cout << "start temperature: " << start_temp << endl;
-	
-	//argument 4 is the netlist filename
-	string filename(argv[4]);
-	cout << "netlist filename: " << filename << endl;
-	
-	//argument 5 (optional) is the number of temperature steps before termination
-	int number_temp_steps = -1;
-        if(argc == 6) {
-		number_temp_steps = atoi(argv[5]);
-		cout << "number of temperature steps: " << number_temp_steps << endl;
-        }
-
-	//now that we've read in the commandline, run the program
-	netlist my_netlist(filename);
-	
-	annealer_thread a_thread(&my_netlist,num_threads,swaps_per_temp,start_temp,number_temp_steps);
-	
-#ifdef ENABLE_PARSEC_HOOKS
-	__parsec_roi_begin();
-#endif
-#ifdef ENABLE_THREADS
-	std::vector<pthread_t> threads(num_threads);
-	void* thread_in = static_cast<void*>(&a_thread);
-	for(int i=0; i<num_threads; i++){
-		pthread_create(&threads[i], NULL, entry_pt,thread_in);
-	}
-	for (int i=0; i<num_threads; i++){
-		pthread_join(threads[i], NULL);
-	}
-#else
-	a_thread.Run();
-#endif
-#ifdef ENABLE_PARSEC_HOOKS
-	__parsec_roi_end();
-#endif
-	
-	cout << "Final routing is: " << my_netlist.total_routing_cost() << endl;
-
-#ifdef ENABLE_PARSEC_HOOKS
-	__parsec_bench_end();
-#endif
-
-	return 0;
-}
-
-void* entry_pt(void* data)
-{
-	annealer_thread* ptr = static_cast<annealer_thread*>(data);
-	ptr->Run();
 }
