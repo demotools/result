11b4f1a8f8c38016df9dd599a40a9c398977c4c9
demotools <quhongliang1984@163.com>
2021-04-09 16:14:52 +0800

    Update run_mytest_all.sh

--- scripts/run_mytest_all.sh
+++ scripts/run_mytest_all.sh
@@ -25,15 +25,15 @@ BENCHMARKS="canneal"
 # BENCHMARKS="sysbench"
 # List of all configs to run
 #CONFIGS="F FM FA FAM I IM"
-CONFIGS="F"
+#CONFIGS="F"
 #CONFIGS="FM"
 #CONFIGS="I"
 #CONFIGS="IM"
 #CONFIGS="F FM I IM"
-#CONFIGS="F FM"
+CONFIGS="F FM"
 #CONFIGS="FM F"
 
-for RUNTIMES in $(seq 1) 
+for RUNTIMES in $(seq 5) 
 do
 	echo "Start test round: $RUNTIMES"
 	echo "Start test round: $RUNTIMES" >> /var/log/syslog 
