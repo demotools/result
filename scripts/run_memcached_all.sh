0e767d1a0e43f768007df49cc7c753b88dd82af3
demotools <quhongliang1984@163.com>
2021-05-15 12:59:49 +0800

    0.6

--- scripts/run_memcached_all.sh
+++ scripts/run_memcached_all.sh
@@ -28,14 +28,14 @@ BENCHMARKS="memcached"
 #CONFIGS="F FM FA FAM I IM"
 #CONFIGS="F"
 #CONFIGS="FM"
-#CONFIGS="I"
+CONFIGS="I"
 #CONFIGS="IM"
-CONFIGS="F FM I IM"
+#CONFIGS="F FM I IM"
 #CONFIGS="F FM"
 #CONFIGS="FM F"
 #CONFIGS="I IM"
 
-for RUNTIMES in $(seq 5) 
+for RUNTIMES in $(seq 1) 
 do
 	echo "Start test round: $RUNTIMES"
 	echo "Start test round: $RUNTIMES" >> /var/log/syslog 
