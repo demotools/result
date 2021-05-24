f60ea22c7482eb73f04ffb96f36b99ce33ba89a9
demotools <quhongliang1984@163.com>
2021-05-24 15:46:09 +0800

    0.6

--- scripts/run_test_all.sh
+++ scripts/run_test_all.sh
@@ -35,7 +35,7 @@ CONFIGS="I"
 #CONFIGS="FM F"
 #CONFIGS="I IM"
 
-for RUNTIMES in $(seq 10) 
+for RUNTIMES in $(seq 1) 
 do
 	echo "Start test round: $RUNTIMES"
 	echo "Start test round: $RUNTIMES" >> /var/log/syslog 
