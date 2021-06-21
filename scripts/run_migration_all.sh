ab255f6175e8e63dfdcf20acca77373233eed3fc
demotools <quhongliang1984@163.com>
2021-06-21 15:16:24 +0800

    0.6

--- scripts/run_migration_all.sh
+++ scripts/run_migration_all.sh
@@ -19,15 +19,16 @@ ROOT=$(dirname `readlink -f "$0"`)
 # List of all benchmarks to run
 # BENCHMARKS="gups btree hashjoin redis xsbench pagerank liblinear canneal"
 #BENCHMARKS="btree"
-BENCHMARKS="hashjoin"
+#BENCHMARKS="hashjoin"
+BENCHMARKS="redis"
 #BENCHMARKS="gups"
 # List of all configs to run
-CONFIGS="LPLD RPILD RPILDM"
-#CONFIGS="LPLD"
+#CONFIGS="LPLD RPILD RPILDM"
+CONFIGS="LPLD"
 #CONFIGS="RPILD"
 #CONFIGS="RPILDM"
 
-for RUNTIMES in $(seq 4) 
+for RUNTIMES in $(seq 1) 
 do
 	echo "Start test round: $RUNTIMES"
 	echo "Start test round: $RUNTIMES" >> /var/log/syslog 
